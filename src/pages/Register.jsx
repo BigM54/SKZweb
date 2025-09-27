529243a8ab5c1fc038be96e7656446a6973a9fca
BigM54 <malo54.raeth@gmail.com>
2025-09-27 12:09:24 +0200

    V1.9

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -162,7 +162,7 @@ export default function RegisterAndVerify() {
       // Ajout des infos cousins si accepté
       if (acceptCousins && !peks) {
         const cousinData = {
-          email,
+          email: mail,
           numero,
           bucque,
         };
