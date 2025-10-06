208dd98fe8729ed3bfe23f39f53dbbd9d185fb28
BigM54 <malo54.raeth@gmail.com>
2025-10-06 17:27:51 +0200

    V1.9

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -205,7 +205,8 @@ export default function RegisterAndVerify() {
       <h2 className="text-center text-primary mb-3">
         {step === 'register' ? 'Créer un compte' : 'Vérifie ton email'}
       </h2>
-
+      <div className="mb-3 text-danger text-center" style={{fontWeight:'bold'}}>Les inscriptions sont en pause pour cause d'un problème technique, nous travaillons pour résoudre au plus vite.</div>
+      <div className="mb-3 text-danger text-center" style={{fontWeight:'bold'}}>Merci pour votre confiance ❤️</div>
       {error && <Alert color="danger">{error}</Alert>}
 
       {step === 'register' && (
@@ -259,7 +260,7 @@ export default function RegisterAndVerify() {
             </FormGroup>
           )}
 
-          <Button color="primary" type="submit" block disabled={loading || !formData.charte}>
+          <Button color="primary" type="submit" block disabled>
             {loading ? <Spinner size="sm" /> : 'Créer mon compte'}
           </Button>
         </Form>
