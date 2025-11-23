4e5bdcfb3c5575955f5f0cd035083c43924a520b
BigM54 <malo54.raeth@gmail.com>
2025-11-23 17:18:46 +0100

    V2

--- src/components/admin/AdminUtilisateurs.jsx
+++ src/components/admin/AdminUtilisateurs.jsx
@@ -130,7 +130,7 @@ export default function AdminUtilisateurs() {
                         <div><strong>🎿 Location :</strong> Pack: {opt.pack_location}, Matériel: {opt.materiel_location}, Casque: {opt.casque}, Assurance: {opt.assurance}</div>
                         <div><strong>🎒 Packs spéciaux :</strong> Fumeur: {opt.pack_fumeur}, Grand Froid: {opt.pack_grand_froid}, Soirée: {opt.pack_soiree}, Masque: {opt.masque}</div>
                         <div><strong>🚌 Bus :</strong> {opt.bus ? opt.bus : '—'} {opt.type_bus ? `(${opt.type_bus})` : ''}</div>
-                        <div><strong>🎟️ Forfait :</strong> {opt.forfait ? opt.forfait : '—'}</div>
+                        <div><strong>🎟️ Forfait :</strong> {opt.type_forfait ? opt.type_forfait : '—'}</div>
                         <div><strong>🎽 Pull :</strong> {opt.taille_pull || '—'}</div>
                         <div><strong>🥗 Régime alimentaire :</strong> {opt.regime || '—'}</div>
                         <div><strong>💵 Paiements :</strong> Acompte : {paiement.acompteStatut ? `✅` : '❌'}, 1 : {paiement.paiement1Statut ? `✅` : '❌'}, 2 : {paiement.paiement2Statut ? `✅` : '❌'}, 3 : {paiement.paiement3Recu ? `✅` : '❌'} {paiement.Fraude ? `FRAUDE ATTENTION` : ''}</div>
